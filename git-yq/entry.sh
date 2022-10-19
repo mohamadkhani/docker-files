9c9d9c628a78151c5d4126f13e82d765cf464acf
Mohammadreza Khani <mohamadkhani14@gmail.com>
2022-10-19 22:27:28 +0330

    feat: fix keys

--- git-yq/entry.sh
+++ git-yq/entry.sh
@@ -1,6 +1,6 @@
 mkdir ~/.ssh
-echo "$SSH_PUB" > ~/.ssh/id_rsa.pub
-echo "$SSH_KEY" | base64 -d > ~/.ssh/id_rsa
+echo $SSH_PUB > ~/.ssh/id_rsa.pub
+echo $SSH_KEY | base64 -d > ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa.pub
 touch ~/.ssh/known_hosts
